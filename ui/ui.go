f732d2ec6d6138143d216a6087182c93127c1682
samtcifihi <la.lojban@gmail.com>
2018-12-01 17:15:22 -0500

    Update Intro Text

--- ui/ui.go
+++ ui/ui.go
@@ -7,6 +7,6 @@ func WelcomeUser() {
 		fmt.Println("")
 	}
 	fmt.Println("Welcome to this LD 43 game of coprime numbers!")
-	fmt.Println("To clear each level you must sacrifice numbers one by one until none are co-prime with eachother.")
+	fmt.Println("To clear each level you must sacrifice numbers one by one until no two numbers in the same row or column are co-prime with eachother unless separated by a boundary.")
 	fmt.Println("If you sacrifice too many numbers, you will fail the level and must begin again.")
 }
